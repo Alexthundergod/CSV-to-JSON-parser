28235baef3de7a637a4d41d69d31dbef4347b281
Oleksandr Zhadovets <100019194+Alexthundergod@users.noreply.github.com>
2024-03-31 21:08:11 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import json
+import csv_to_json_parser as cjp
+
+csv_file = r"csv_file.csv"
+json_file = r"json_file.json"
+
+cjp.parser(csv_file, json_file)
+
+with open(json_file) as f:
+    datadict = json.load(f)
+
+datalist = []
+
+for inner_dict in datadict.values():
+    for key, value in inner_dict.items():
+        if key == "Interacting aminoacid":
+            datalist.append(value)
+
+
+N, bins, patches = plt.hist(datalist, bins=len(set(datalist)))
+fracs = N / N.max()
+norm = colors.Normalize(fracs.min(), fracs.max())
+
+for thisfrac, thispatch in zip(fracs, patches):
+    color = plt.cm.viridis(norm(thisfrac))
+    thispatch.set_facecolor(color)
+
+plt.title("The number of bonds that each amino acid forms")
+plt.xlabel("Aminoacid")
+plt.ylabel("N of bonds")
+
+plt.show()
